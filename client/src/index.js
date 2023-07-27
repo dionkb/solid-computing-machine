3bcdace6146fb122aecdb986fa55ae816093af8c
dionkb <dionkbaskara@gmail.com>
2023-07-26 20:50:10 -0400

    see comment

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Below added for additional Redux code
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
 
+// Instantiating the store
+const store = configureStore()
+
+// Swapped starter React wrap for Provider with the store passed in
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
